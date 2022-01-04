92481055d5918297c31732c74e4aa4feeb3144c8
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-04 22:10:01 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -24,24 +24,4 @@ function answerBot(){
 }
 answerBot();
 
-$keyboard = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-         ['0']
-];
-
-$reply_markup = $telegram->replyKeyboardMarkup([
-    'keyboard' => $keyboard, 
-    'resize_keyboard' => true, 
-    'one_time_keyboard' => true
-]);
-
-$response = $telegram->sendMessage([
-    'chat_id' => $chat_id, 
-    'text' => 'Hello World', 
-    'reply_markup' => $reply_markup
-]);
-
-$messageId = $response->getMessageId();
 ?>
