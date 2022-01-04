9f01cdac165bf514ed14c3cd3fce3a3d62cee42b
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-04 20:26:22 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -3,15 +3,25 @@
 define('TOKEN', getenv('token'));
 define('API','https://api.telegram.org/bot'.TOKEN.'/');
 
+function answerBot(){
+    $data = json_decode(file_get_contents('php://input'));
 
-$result = file_get_contents(API.'getUpdates?');
-print_r($result);
+    $Text = $data->message->text;
 
-/*
-    $result = file_get_contents(API.'sendMessage?'.http_build_query([
+    if($Text == 'Привет'){
+        $result = file_get_contents(API.'sendMessage?'.http_build_query([
             'chat_id' => $data->message->chat->id,
-            'text' => $data->message->text
-    ]));
-*/
+            'text' => 'и тебе Привет'
+        ]));
+    }elseif($Text != 'Привет'){
+        $result = file_get_contents(API.'sendMessage?'.http_build_query([
+            'chat_id' => $data->message->chat->id,
+            'text' => 'Не могу понять что вы написали, напишите Привет.'
+        ]));
+    }
+
+}
+answerBot();
+
 
 ?>
