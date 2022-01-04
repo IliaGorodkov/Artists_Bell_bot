78e3f1051f14bfc2427cb7559f4f1d4e8729cfc3
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-04 16:03:13 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -1,23 +1,23 @@
 <?php
 
 //$result = file_get_contents('https://api.telegram.org/bot2009982557:AAGwlzdC8DbZ9c3lRu0kFDwjbkoPn-gUz9s/getMe');
-/*
-$query = curl_init('https://api.telegram.org/bot2009982557:AAGwlzdC8DbZ9c3lRu0kFDwjbkoPn-gUz9s/sendMessage?chat_id=1307855636&text=Hello');
+
+define('TOKEN', getenv('token'));
+//define('API','https://api.telegram.org/bot'.TOKEN.'/');
+
+$query = curl_init('https://api.telegram.org/bot'.TOKEN.'/');
 curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($query, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($query, CURLOPT_HEADER, false);
-$result = curl_exec($query);
+$resultCURL = curl_exec($query);
 curl_close($query);
- 
-print_r($result);
-*/
 
-define('TOKEN', getenv('token'));
-define('API','https://api.telegram.org/bot'.TOKEN.'/');
+
+
 //print_r(API);
 $data = json_decode(file_get_contents('php://input'));
 
-$result = file_get_contents(API.'sendMessage?'.http_build_query([
+$result = file_get_contents($resultCURL.'sendMessage?'.http_build_query([
         'chat_id' => $data->message->chat->id,
         'text' => $data->message->text
     ]));
