f66f319b946bd56d78d42b5affff13f00df64c42
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-04 16:41:39 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -1,19 +1,8 @@
 <?php
 
-//$result = file_get_contents('https://api.telegram.org/bot2009982557:AAGwlzdC8DbZ9c3lRu0kFDwjbkoPn-gUz9s/getMe');
-
 define('TOKEN', getenv('token'));
 define('API','https://api.telegram.org/bot'.TOKEN.'/');
-/*
-$query = curl_init('https://api.telegram.org/bot'.TOKEN.'/');
 
-curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($query, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($query, CURLOPT_HEADER, false);
-$resultCURL = curl_exec($query);
-
-curl_close($query);
-*/
 function ResponseBot(){
     $data = json_decode(file_get_contents('php://input'));
 
@@ -21,24 +10,16 @@ function ResponseBot(){
             'chat_id' => $data->message->chat->id,
             'text' => $data->message->text
     ]));
+    printf($result.'/-/');
+    print_r($result.'/--/');
+    if($result['text'] == 'Привет'){
+        return 'и тебе Привет';
+    }
+
 
         return $result;
 }
 
 echo ResponseBot();
-
-
-//print_r(API);
-/*
-$data = json_decode(file_get_contents('php://input'));
-
-$result = file_get_contents($resultCURL.'sendMessage?'.http_build_query([
-        'chat_id' => $data->message->chat->id,
-        'text' => $data->message->text
-    ]));
-print_r($result);
-*/
-
-
 
 ?>
