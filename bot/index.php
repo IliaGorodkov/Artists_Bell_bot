802dded28124463cd056705747b659aba3e5cd72
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-04 21:47:28 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -6,22 +6,38 @@ define('API','https://api.telegram.org/bot'.TOKEN.'/');
 function answerBot(){
     $data = json_decode(file_get_contents('php://input'));
 
+    $chat_id = $data->message->chat->id;
     $Text = $data->message->text;
     $Name = $data->message->from->first_name;
 
     if($Text=='Привет'||$Text=='привет'){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
-            'chat_id' => $data->message->chat->id,
+            'chat_id' => $chat_id,
             'text' => 'И тебе Привет '. $Name
         ]));
     }elseif($Text=='Пока'||$Text=='пока'||$Text=='Пака'||$Text=='пака'||$Text=='ББ'||$Text=='бб'||$Text=='Досвидос'){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
-            'chat_id' => $data->message->chat->id,
+            'chat_id' => $chat_id,
             'text' => 'Досвидания '. $Name
         ]));
     }
-
 }
 answerBot();
+
+
+$replyMarkup = array(
+    'keyboard' => array(
+        array("A", "B")
+    )
+);
+$encodedMarkup = json_encode($replyMarkup);
+$content = array(
+    'chat_id' => $chat_id,
+    'reply_markup' => $encodedMarkup,
+    'text' => "Test"
+);
+
+
+
 
 ?>
