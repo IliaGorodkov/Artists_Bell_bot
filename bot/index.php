f55482cf3303cca32027b6cd31ffa6facae0c045
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-06 05:16:03 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -13,26 +13,30 @@ function answerBot(){
 
 
 
-    if($Text=="/help"){
+    if($Text=="/start"){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
             'chat_id' => $chat_id,
             'text' => "Привет, $first_name, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
+        ]));
+    }elseif($Text=="/help"){
+        $result = file_get_contents(API.'sendMessage?'.http_build_query([
+            'chat_id' => $chat_id,
+            'text' => "$first_name, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
         ]));
     }elseif($Text=="/about"){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
             'chat_id' => $chat_id,
             'text' => "Этого Бота создал Программист-Красавчик Илья \xF0\x9F\x98\x81"
         ]));
-    }
-    elseif($Text=='Пока'||$Text=='пока'||$Text=='Пака'||$Text=='пака'||$Text=='ББ'||$Text=='бб'||$Text=='Досвидос'){
-        $result = file_get_contents(API.'sendMessage?'.http_build_query([
-            'chat_id' => $chat_id,
-            'text' => 'Досвидания '. $first_name
-        ]));
     }elseif($Text=='Привет'||$Text=='привет'){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
             'chat_id' => $chat_id,
-            'text' => 'И тебе Привет '. $first_name
+            'text' => 'И тебе Привет '.$first_name."\xE2\x9C\x8C"
+        ]));
+    }elseif($Text=='Пока'||$Text=='пока'||$Text=='Пака'||$Text=='пака'||$Text=='ББ'||$Text=='бб'||$Text=='Досвидос'){
+        $result = file_get_contents(API.'sendMessage?'.http_build_query([
+            'chat_id' => $chat_id,
+            'text' => 'Досвидания '.$first_name."\xF0\x9F\x91\x8B"
         ]));
     }
 
