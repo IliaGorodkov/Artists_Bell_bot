3af9054dfd2c78571eae4ea794d6cd449e6c473b
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-08 23:42:34 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -3,13 +3,6 @@
 define('TOKEN', getenv('token'));
 define('API','https://api.telegram.org/bot'.TOKEN.'/');
 
-
-
-$data = json_decode(file_get_contents('php://input'));
-$chat_id = $data->message->chat->id;
-$Text = $data->message->text;
-$first_name = $data->message->from->first_name;
-
 function answerBot(){
     $data = json_decode(file_get_contents('php://input'));
 
@@ -24,12 +17,23 @@ function answerBot(){
             ]
         ]
     ];
-    $encodedKeyboard = json_encode($keyboard);
+    $encodedKeyboard = json_encode($keyboard,true);
+    
+    $buttons = json_encode([
+        "keyboard" => [[
+            ["text" => "Button 1_1",],
+            ["text" => "Button 1_2",]
+        ],
+        'one_time_keyboard' => false,
+        'resize_keyboard' => true,
+        'selective' => true,], true
+    ]);
 
     if($Text=="/start"||$Text=='Привет'||$Text=='привет'){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
             'chat_id' => $chat_id,
-            'text' => "Привет, $first_name \xF0\x9F\x91\x8B, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
+            'text' => "Привет, $first_name \xF0\x9F\x91\x8B, вот команды, что я понимаю:\n/help - список команд \n/about - о нас",
+            'reply_markup' => $buttons
         ]));
     }elseif($Text=="/help"){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
@@ -59,27 +63,6 @@ function answerBot(){
 
 }
 answerBot();
-
-
-
-function send($method, $data)
-{
-    $url = "https://api.telegram.org/bot2009982557:AAGwlzdC8DbZ9c3lRu0kFDwjbkoPn-gUz9s". "/" . $method;
-    
-    $data = json_decode(file_get_contents('php://input'));
-    $chat_id = $data->message->chat->id;
-    $Text = $data->message->text;
-    $first_name = $data->message->from->first_name;
-
-    $result = file_get_contents(API.'sendMessage?'.http_build_query([
-        'chat_id' => $chat_id,
-        'text' => 'Досвидания '.$first_name."\xF0\x9F\x91\x8B",'reply_markup' => $encodedKeyboard
-    ]));
-    return  $result;
-}
-
-
-
 
 
 
