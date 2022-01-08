e2e3ffd4941ad6265c895e36052402e879886a2b
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-08 22:40:24 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -17,18 +17,26 @@ function answerBot(){
     $Text = $data->message->text;
     $first_name = $data->message->from->first_name;
 
-
-
+    $keyboard = [
+        'inline_keyboard' => [
+            [
+                ['text' => 'forward me to groups', 'callback_data' => 'someString']
+            ]
+        ]
+    ];
+    $encodedKeyboard = json_encode($keyboard);
 
     if($Text=="/start"||$Text=='Привет'||$Text=='привет'){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
             'chat_id' => $chat_id,
-            'text' => "Привет, $first_name \xF0\x9F\x91\x8B, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
+            'text' => "Привет, $first_name \xF0\x9F\x91\x8B, вот команды, что я понимаю:\n/help - список команд \n/about - о нас",
+            'reply_markup' => $encodedKeyboard
         ]));
     }elseif($Text=="/help"){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
             'chat_id' => $chat_id,
-            'text' => "$first_name, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
+            'text' => "$first_name, вот команды, что я понимаю:\n/help - список команд \n/about - о нас",
+            'reply_markup' => $encodedKeyboard
         ]));
     }elseif($Text=="/about"){
         $result = file_get_contents(API.'sendMessage?'.http_build_query([
@@ -54,37 +62,21 @@ function answerBot(){
 answerBot();
 
 
-$keyboard = [
-    'inline_keyboard' => [
-        [
-            ['text' => 'forward me to groups', 'callback_data' => 'someString']
-        ]
-    ]
-];
-$encodedKeyboard = json_encode($keyboard);
-$parameters = 
-    array(
-        'chat_id' => $chat_id, 
-        'text' => $response, 
-        'reply_markup' => $encodedKeyboard
-    );
-
-send('sendMessage', $parameters); // function description Below
 
 function send($method, $data)
 {
     $url = "https://api.telegram.org/bot2009982557:AAGwlzdC8DbZ9c3lRu0kFDwjbkoPn-gUz9s". "/" . $method;
+    
+    $data = json_decode(file_get_contents('php://input'));
+    $chat_id = $data->message->chat->id;
+    $Text = $data->message->text;
+    $first_name = $data->message->from->first_name;
 
-    if (!$curld = curl_init()) {
-        exit;
-    }
-    curl_setopt($curld, CURLOPT_POST, true);
-    curl_setopt($curld, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($curld, CURLOPT_URL, $url);
-    curl_setopt($curld, CURLOPT_RETURNTRANSFER, true);
-    $output = curl_exec($curld);
-    curl_close($curld);
-    return $output;
+    $result = file_get_contents(API.'sendMessage?'.http_build_query([
+        'chat_id' => $chat_id,
+        'text' => 'Досвидания '.$first_name."\xF0\x9F\x91\x8B",'reply_markup' => $encodedKeyboard
+    ]));
+    return  $result;
 }
 
 
