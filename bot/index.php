f643a79d1e97e635faf0d13a6bef882b3783e3a9
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-01-09 01:55:49 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -39,29 +39,24 @@ function answerBot(){
 
 
     if($Text=="/start"||$Text=='Привет'||$Text=='привет'){
-        $result = file_get_contents(API.'sendMessage?'.http_build_query([
-            'chat_id' => $chat_id,
-            'text' => "Привет, $first_name \xF0\x9F\x91\x8B, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
-        ]));
+        $this->botApiQuery("sendMessage",[
+        'chat_id' => $chat_id,
+        'text' => "Привет, $first_name \xF0\x9F\x91\x8B, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
+        ]);
     }elseif($Text=="/help"){
-        $result = file_get_contents(API.'sendMessage?'.http_build_query([
-            'chat_id' => $chat_id,
-            'text' => "$first_name, вот команды, что я понимаю:\n/help - список команд \n/about - о нас"
-        ]));
+        $this->botApiQuery("sendMessage",[
+        'chat_id' => $chat_id,
+        'text' => "$first_name, вот команды, что я понимаю:\n/help - список команд \n/about - о Создателе Бота"
+        ]);
     }elseif($Text=="/about"){
-        $result = file_get_contents(API.'sendMessage?'.http_build_query([
-            'chat_id' => $chat_id,
-            'text' => "Этого Бота создал Программист-Красавчик Илья \xF0\x9F\x98\x81",
-            'reply_markup' => $encodedKeyboard
-        ]));
+        $this->botApiQuery("sendMessage",[
+        'chat_id' => $chat_id,
+        'text' => "Этого Бота создал Программист-Красавчик Илья \xF0\x9F\x98\x81",
+        ]);
     }elseif($Text=='Пока'||$Text=='пока'||$Text=='Пака'||$Text=='пака'||$Text=='ББ'
     ||$Text=='бб'||$Text=='Досвидания'||$Text=='досвидания'||$Text=='Досвидос'||$Text=='досвидос'){
-        /*$result = file_get_contents(API.'sendMessage?'.http_build_query([
-            'chat_id' => $chat_id,
-            'text' => 'Досвидания '.$first_name."\xF0\x9F\x91\x8B"
-        ]));*/
-        
-        $this->botApiQuery("sendMessage", ['chat_id' => $chat_id,
+        $this->botApiQuery("sendMessage",[
+        'chat_id' => $chat_id,
         'text' => 'Досвидания '.$first_name."\xF0\x9F\x91\x8B"
         ]);
     }
@@ -91,5 +86,5 @@ function botApiQuery($method, $fields = array()){
 
 $Bott = new Bot;
 $Bott->answerBot();
-
+//'reply_markup' => $encodedKeyboard
 ?>
