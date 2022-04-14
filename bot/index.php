85ee61ea3c322710cbb123ae6c5b06373a406004
IliaGorodkov <65397259+IliaGorodkov@users.noreply.github.com>
2022-04-14 23:41:11 +0300

    Add files via upload

--- bot/index.php
+++ bot/index.php
@@ -36,7 +36,7 @@ function answerBot(){
 
 
     $mysqli = new mysqli($host, $db, $user, $pass);
-    if ($Text || $mysqli->connect_errno) {
+    if ($Text=='123') {
         $this->botApiQuery("sendMessage",[
         'chat_id' => "1307855636",
         'text' => "Не удалось подключиться к БД ('.$mysqli->connect_errno.': '.$mysqli->connect_error.') для пользователя с user_id: .$chat_id"
